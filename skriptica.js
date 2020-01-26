2b7f3e0eb4b00edb7bbd5fb0cc74b5c059baf733
Uros <40537592+urosbmx@users.noreply.github.com>
2020-01-26 22:42:31 +0100

    doda je ewe ski

--- skriptica.js
+++ skriptica.js
@@ -1,5 +1,5 @@
-$("#save").click(function() {
-    $(".podaci").each(function() {
+$("#save").click(function () {
+    $(".podaci").each(function () {
         var podaci = $(this).val();
         // console.log(podaci);
         var ime = $("input[name=name]").val();
@@ -15,7 +15,7 @@ $("#save").click(function() {
                 EMAIL: email,
                 MESAGE: text,
             },
-            success: function(data) {
+            success: function (data) {
                 console.log(data);
                 alert("Uspesno ste poslali podatke");
                 $("input[name=name]").val("");
@@ -28,38 +28,78 @@ $("#save").click(function() {
 });
 
 //prikazivanje modala
-$("[data-group='Companies']").on("click",function(){
+$("[data-group='Companies']").on("click", function () {
     $("#modalholder").show();
 });
 
 // zatvaranje modala
-$(".close").on("click",function(){
+$(".close").on("click", function () {
     $("#modalholder").hide();
 });
 
 // logovanje na formu
-$("#login").on("click",function(){
-    $("#holderLogin").find("input").each(function(){
+$("#login").on("click", function () {
+    $("#holderLogin").find("input").each(function () {
         var vrednost = $(this).val();
         var username = $("#username").val();
         var password = $("#password").val();
 
-        if(username =="uros" && password == "katanic"){
-            window.open('error404.html');
+        if (username == "uros" && password == "katanic") {
+
             $("#modalholder").hide();
-           $("#username").val("");
+            $("#username").val("");
             $("#password").val("");
             $("#username").removeClass("error");
             $("#password").removeClass("error");
+            window.open('error404.html');
         }
-        else{
+        else {
             $("#username").addClass("error");
             $("#password").addClass("error");
             $(".greskica").show();
-    }
+        }
 
     });
-   
+
+});
+
+//eweskiforma
+
+$("#prijaviEwe").on('click', function () {
+
+    var name = $("#email-ski-name").val();
+    var lastName = $("#email-ski-lastname").val();
+    var email = $("#email-ski").val();
+    var companie = $("#email-ski-company").val();
+        if (name == "" && lastName == "" && email == "" && companie == "") {
+            $("input").addClass('error');
+
+        } else {
+
+            $("input").removeClass('error');
+            name = $("#email-ski-name").val("");
+            lastName = $("#email-ski-lastname").val("");
+            email = $("#email-ski").val("");
+            companie = $("#email-ski-company").val("");
+
+            $.ajax({
+                type: "post",
+                url: "get.php",
+                data: "data",
+                dataType: "dataType",
+                success: function (response) {
+                    
+                }
+            });
+            
+        }
+
+
+
+
+
+
+
 });
 
 
